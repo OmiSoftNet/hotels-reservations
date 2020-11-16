9c51865762bccd1335fa120238911d09a919cca8
Fedir Pyshnyi <fed.lviv@gmail.com>
2020-11-16 12:52:13 +0200

    Initial

---
+++ src/main/java/net/omisoft/hotel/HotelsReservationsApplication.java
@@ -0,0 +1,13 @@
+package net.omisoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsReservationsApplication.class, args);
+    }
+
+}
